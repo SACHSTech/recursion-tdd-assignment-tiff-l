f906ae7ad2bcc392b8705cfa874a1f4d50b1462b
Tiffany Lee <>
2021-05-21 22:56:37 +0000

    added comments/test more test cases (final submission)

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -3,11 +3,13 @@ package recursionAssignment;
 public class Recursion{
 
   public static int countHi(String str){
-
-    if(str.equals("")) {
+    // Base case
+    if(str.length() < 2) {
       return 0; 
-    } else if (str.substring(0,1).equals("h")&& str.substring(1,2).equals("i")){
+    // Recursive step - "hi"
+    } else if (str.substring(0,2).equals("hi")){
       return 1 + countHi(str.substring(2));
+    //Recursive step - no "hi"
     }else{
       return countHi(str.substring(1));
     }
@@ -15,29 +17,31 @@ public class Recursion{
   }
 
   public static String changePi(String str){
-
+    //Base Case
     if(str.length() < 2){
       return str; 
     } 
-
-    if(str.substring(0,2).equals("pi")){
+    //Recursive step: "pi"
+    if (str.substring(0,2).equals("pi")){
       return "3.14" + changePi(str.substring(2));
     }
-
+    //Recursive step: no "pi"
     return str.charAt(0) + changePi(str.substring(1));
 
   }
 
 
-
-
   public static String stringClean(String str){
+    //Base case
     if(str.equals("")){
       return ""; 
+    //Recursive step: 1 letter
     } else if(str.substring(1).equals("")){
       return str.substring(0,1) + stringClean(str.substring(1));
+    // Recursive step: first and second character are the same. 
     } else if(str.substring(0,1).equals(str.substring(1,2))){
       return stringClean(str.substring(1));
+    // Recursive step: first and second character are different
     } else{
       return str.substring(0,1)+ stringClean(str.substring(1)); 
     }
--- src/recursionAssignment/RecursionTest.java
+++ src/recursionAssignment/RecursionTest.java
@@ -6,59 +6,126 @@ import recursionAssignment.Recursion;
 
 public class RecursionTest{
 
-    // Problem Set 1
-
+    /** Problem Set 1
+     *Test base case
+     */
     @Test
-    public void Test1(){
+    public void Test1_1(){
         assertEquals(0, Recursion.countHi(""));
     }
 
+    /**
+     * Testing recursive step 
+     */
     @Test
-    public void Test2(){
+    public void Test1_2(){
       assertEquals(1, Recursion.countHi("xxhixx"));
     }
 
+    /**
+     * Testing recursive step 
+     */
     @Test
-    public void Test3(){
+    public void Test1_3(){
       assertEquals(2, Recursion.countHi("xhixhix"));
     }
 
-    // Problem Set 2
-
+    /**
+     * Testing recursive step 
+     */
     @Test
-    public void Test4(){
+    public void Test1_4(){
+      assertEquals(4, Recursion.countHi("ihhihihiihi"));
+    }
+
+    /**
+     * Testing recursive step 
+     */
+    @Test
+    public void Test1_5(){
+      assertEquals(1, Recursion.countHi("hi"));
+    }
+
+
+    /** Problem Set 2
+    *Test base case
+    */
+    @Test
+    public void Test2_1(){
       assertEquals("", Recursion.changePi(""));
     }
 
+     /**
+     * Testing base case 
+     */
     @Test
-    public void Test5(){
-      assertEquals("3.14", Recursion.changePi("pi"));
+    public void Test2_2(){
+      assertEquals("p", Recursion.changePi("p"));
     }
 
+    
+    /**
+     * Testing recursive step 
+     */
     @Test
-    public void Test6(){
+    public void Test2_3(){
       assertEquals("3.14p", Recursion.changePi("pip"));
     }
 
+    /**
+     * Testing recursive step 
+     */
     @Test
-    public void Test7(){
+    public void Test2_4(){
       assertEquals("3.143.14", Recursion.changePi("pipi"));
     }
 
-    //Problem set 3
+    /**
+     * Testing recursive step 
+     */
     @Test
-    public void Test8(){
+    public void Test2_5(){
+      assertEquals("3.14", Recursion.changePi("pi"));
+    }
+
+    /** Problem Set 3
+    *Test base case
+    */
+    @Test
+    public void Test3_1(){
       assertEquals("yza", Recursion.stringClean("yyzzza"));
     }
 
+    /**
+     * Problem Set 3: Testing recursive step 
+     */
     @Test
-    public void Test9(){
+    public void Test3_2(){
       assertEquals("abcd", Recursion.stringClean("abbbcdd"));
     }
 
+    /**
+     * Problem Set 3: Testing recursive step 
+     */
     @Test
-    public void Test10(){
+    public void Test3_3(){
       assertEquals("Helo", Recursion.stringClean("Hello"));
+    }
+
+    /**
+     * Problem Set 3: Testing recursive step 
+     */
+    @Test
+    public void Test3_4(){
+      assertEquals("x", Recursion.stringClean("xxxxxx"));
+    }
+
+    /**
+     * Problem Set 3: Testing recursive step 
+     */
+    @Test
+    public void Test3_5(){
+      assertEquals("yayayay", Recursion.stringClean("yayayayy"));
     }
 
 }
